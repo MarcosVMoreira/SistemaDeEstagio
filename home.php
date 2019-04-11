46d63eb5d257c1461f3980165d6f28b4c993f72b
Felipe Borges da Silva <felipebsilva5@gmail.com>
2019-04-11 14:14:03 -0300

    Consistência de login

--- home.php
+++ home.php
@@ -9,22 +9,56 @@
 	(isset($_SESSION['nome']) && $_SESSION['nome'] != "") && (isset($_SESSION['senha']) && $_SESSION['senha'] != ""))) {
         header("Location: login.php");
 	} else {
+        if ((isset($_SESSION['rg']) && $_SESSION['rg'] != "")){
+            $rg = $_SESSION['rg'];
+        }
+        if ((isset($_SESSION['cpf']) && $_SESSION['cpf'] != "")){
+            $cpf = $_SESSION['cpf'];
+        }
         if ((isset($_SESSION['nome']) && $_SESSION['nome'] != "")){
             $nomeAluno = $_SESSION['nome'];
             $temp = explode(" ",$nomeAluno);
             $nomeNovo = $temp[0] . " " . $temp[count($temp)-1];
         }
+        if ((isset($_SESSION['cidade']) && $_SESSION['cidade'] != "")){
+            $cidade = $_SESSION['cidade'];
+        }
+        if ((isset($_SESSION['uf']) && $_SESSION['uf'] != "")){
+            $uf = $_SESSION['uf'];
+        }
+        if ((isset($_SESSION['cep']) && $_SESSION['cep'] != "")){
+            $cep = $_SESSION['cep'];
+        }
+        if ((isset($_SESSION['endereco']) && $_SESSION['endereco'] != "")){
+            $endereco = $_SESSION['endereco'];
+        }
+        if ((isset($_SESSION['bairro']) && $_SESSION['bairro'] != "")){
+            $bairro = $_SESSION['bairro'];
+        }
+        if ((isset($_SESSION['numero']) && $_SESSION['numero'] != "")){
+            $numero = $_SESSION['numero'];
+        }
         if ((isset($_SESSION['curso']) && $_SESSION['curso'] != "")){
             $curso = $_SESSION['curso'];
         }
-        if ((isset($_SESSION['email']) && $_SESSION['email'] != "")){
-            $email = $_SESSION['email'];
+        if ((isset($_SESSION['campus']) && $_SESSION['campus'] != "")){
+            $campus = $_SESSION['campus'];
         }
         if ((isset($_SESSION['ra']) && $_SESSION['ra'] != "")){
             $ra = $_SESSION['ra'];
         }
-        
-
+        if ((isset($_SESSION['email']) && $_SESSION['email'] != "")){
+            $email = $_SESSION['email'];
+        }
+        if ((isset($_SESSION['dataNascimento']) && $_SESSION['dataNascimento'] != "")){
+            $dataNascimento = $_SESSION['dataNascimento'];
+        }
+        if ((isset($_SESSION['periodoAno']) && $_SESSION['periodoAno'] != "")){
+            $periodoAno = $_SESSION['periodoAno'];
+        }
+        if (isset($_SESSION['complemento'])){
+            $complemento = $_SESSION['complemento'];
+        }
 
 ?>
 
@@ -41,45 +75,45 @@
                 <tbody>
                     <tr>
                         <th class="alinhar">Nome Completo</th>
-                        <td>Otávio Messias Palma</td>
+                        <td><?php echo($nomeAluno) ?></td>
                         <th class="alinhar">Data de Nascimento</th>
-                        <td>06/04/1998</td>
+                        <td><?php echo($dataNascimento)?></td>
                     </tr>
                     <tr>
                         <th class="alinhar">CPF</th>
-                        <td>123.456.789-01</td>
+                        <td><?php echo($cpf)?> </td>
                         <th class="alinhar">RG</th>
-                        <td>12.345.678-9</td>
+                        <td><?php echo($rg) ?></td>
                     </tr>
                     <tr>
                         <th class="alinhar">Email</th>
                         <td><?php echo($email); ?> </td>
                         <th class="alinhar">Matrícula (R.A.)</th>
-                        <td>12345678901</td>
+                        <td><?php echo($ra) ?> </td>
                     </tr>
                     <tr>
                         <th class="alinhar">Curso</th>
-                        <td>Engenharia de Computação</td>
+                        <td><?php echo($curso) ?> </td>
                         <th class="alinhar">Módulo/Ano</th>
-                        <td>7/4</td>
+                        <td><?php echo($periodoAno) ?></td>
                     </tr>
                     <tr>
                         <th class="alinhar">CEP</th>
-                        <td>37700-000</td>
+                        <td><?php echo($cep) ?></td>
                         <th class="alinhar">Bairro</th>
-                        <td>Centro</td>
+                        <td><?php echo($bairro) ?></td>
                     </tr>
                     <tr>
                         <th class="alinhar">Endereço</th>
-                        <td>Avenida Presidente João, 50</td>
+                        <td><?php echo($endereco . ", " . $numero) ?></td>
                         <th class="alinhar">Complemento</th>
-                        <td></td>
+                        <td><?php echo($complemento) ?></td>
                     </tr>
                     <tr>
                         <th class="alinhar">Estado</th>
-                        <td>Minas Gerais</td>
+                        <td><?php echo($uf) ?></td>
                         <th class="alinhar">Cidade</th>
-                        <td>Poços de Caldas</td>
+                        <td><?php echo($cidade) ?></td>
                     </tr>
                 </tbody>
             </table>
--- loginValidar.php
+++ loginValidar.php
@@ -11,11 +11,28 @@
             $_SESSION['loginErro'] = "Usuário ou senha inválidos.";
             header("Location: login.php");
         } else {
-            $_SESSION['senha'] = $resultado["senha"];
-            $_SESSION['ra'] =  $resultado["ra"];
+            $_SESSION['rg'] = $resultado["rg"];
+            $_SESSION['cpf'] = $resultado["cpf"];
             $_SESSION['nome'] = $resultado["nome"];
+            $_SESSION['cidade'] = $resultado["cidade"];
+            $_SESSION['uf'] = $resultado["uf"];
+            $_SESSION['cep'] = $resultado["cep"];
+            $_SESSION['endereco'] = $resultado["endereco"];
+            $_SESSION['bairro'] = $resultado["bairro"];
+            $_SESSION['numero'] = $resultado["numero"];
             $_SESSION['curso'] = $resultado["curso"];
+            $_SESSION['campus'] = $resultado["campus"];
+            $_SESSION['ra'] =  $resultado["ra"];
+            $_SESSION['telefoneFixo'] = $resultado["telefoneFixo"];
+            $_SESSION['telefoneCelular'] = $resultado["telefoneCelular"];
             $_SESSION['email'] = $resultado["email"];
+            $_SESSION['dataNascimento'] = $resultado["dataNascimento"];
+
+            $_SESSION['periodoAno'] = $resultado["periodoAno"];
+
+            $_SESSION['complemento'] = $resultado["complemento"];
+            $_SESSION['senha'] = $resultado["senha"];
+                        
             header("Location: home.php");
         }
     }
